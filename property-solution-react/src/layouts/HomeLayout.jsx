99f9d74d6237643cdb7ca4a9518178eab58b92b0
DASH-SIMANTA <cse.simanta.dash@gmail.com>
2025-08-11 17:09:59 +0600

    update
    components, layouts, routes

---
+++ property-solution-react/src/layouts/HomeLayout.jsx
@@ -0,0 +1,14 @@
+import Navbar from "../components/Navbar";
+
+
+const HomeLayout = () => {
+    return (
+        <div>
+            <nav>
+                <Navbar></Navbar>
+            </nav>
+        </div>
+    );
+};
+
+export default HomeLayout;
